ed55fc1878b89aafddc4869f131212aa0c475fe4
Anastacia Batarey <batarey.work@gmail.com>
2017-03-10 04:04:44 +0300

    Refactor support.html and styles

--- app/assets/javascripts/vendor.js
+++
@@ -1,3 +0,0 @@
-//=require../../../node_modules/angular/angular.min.js
-//= require ../../../node_modules/jquery/dist/jquery.min.js
-//= require ../../../node_modules/jquery-modal/jquery.modal.min.js
